49a1ac18d08a66efc61e996edc803c3933ed80b6
Javito774 <32337588+Javito774@users.noreply.github.com>
2020-03-26 11:22:52 +0100

    Crear Clase Ventana Corregido
    Existian errores en el metodo cambiarEstado

---
+++ clases/Ventana.js
@@ -0,0 +1,18 @@
+class Ventana
+{
+	constructor(idAux,nombre)
+	{
+		super(idAux,nombre,"cerrada","icono");
+		this.icono2 = "icono2";
+	}
+	cambiarEstado()
+	{
+		var aux = this.icono;
+		this.icono = this.icono2;
+		this.icono2 = aux;
+		if(this.estado = "abierta")
+			this.estado = "cerrada";
+		else
+			this.estado = "abierta";
+	}
+}
