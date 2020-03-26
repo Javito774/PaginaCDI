6863057ce9a2ebfddf46f2756ed7ab6ce90c9276
Javito774 <32337588+Javito774@users.noreply.github.com>
2020-03-26 11:28:30 +0100

    Crear Clase Persiana
    Implementación de la clase persiana

---
+++ clases/Persiana.js
@@ -0,0 +1,18 @@
+class Persiana
+{
+	constructor(idAux,nombre)
+	{
+		super(idAux,nombre,"bajada","icono");
+		this.icono2 = "icono2";
+	}
+	cambiarEstado()
+	{
+		var aux = this.icono;
+		this.icono = this.icono2;
+		this.icono2 = aux;
+		if(this.estado = "subida")
+			this.estado = "bajada";
+		else
+			this.estado = "subida";
+	}
+}
