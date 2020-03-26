5c4b789099e5b00da1f69b6e0edccf8b03f1841d
Javito774 <32337588+Javito774@users.noreply.github.com>
2020-03-26 19:08:54 +0100

    Implementacion de la biblioteca Multimedia

--- clases/BibliotecaMultimedia.js
+++ clases/BibliotecaMultimedia.js
@@ -1,7 +1,17 @@
 class BibliotecaMultimedia
 {
-	constructor()
+	constructor(nombre)
 	{
-		
+		this.nombre = nombre;
+		this.peliculas = [];
+	}
+	añadirPelicula(pelicula)
+	{
+		this.peliculas.push(pelicula);
+	}
+	eliminarPelicula(numero)
+	{
+		if(numero>=0 && numero<this.peliculas.length)
+			this.peliculas.splice(numero,1);
 	}
 }
