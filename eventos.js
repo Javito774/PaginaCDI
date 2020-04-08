d81437e729fb86752a9aee23bf6b4533323c034d
Javito774 <32337588+Javito774@users.noreply.github.com>
2020-04-08 11:43:01 +0200

    Add files via upload

--- eventos.js
+++ eventos.js
@@ -1,3 +1,7 @@
+var pantallas = []
+
+
+
 //@TODO organizar distintos temporizadores para que no tengan el mismo nombre.
 
 //CONTROL DEL RELOJ
@@ -27,39 +31,68 @@ function mostrarMenu(numero)
 	var pantalla1 = document.querySelector("#controlesSilla");
 	var pantalla2 = document.querySelector("#PantallaSecundaria");
 	var menuElem = document.querySelectorAll(".categoria-menu");
-
-	if(menuNum!=-1)
-	{
-			menuElem[menuNum].style.opacity="0";
-			menuElem[menuNum].style.zIndex="2";
-	}
 	if(menuNum == 0)
 	{
 		desactivarmapa();
 	}
 	if(menuNum == numero)
 	{
+		menuElem[menuNum].style.display="none";
 		menuNum = -1;
 		pantalla1.style.width="100%";
 		pantalla2.style.width="0%";
+		pantallas = [];
 	}
-
 	else
 	{
-		menuNum = numero;
-		pantalla1.style.width="50%";
-		pantalla2.style.width="50%";
-		menuElem[menuNum].style.opacity="1";
-		menuElem[menuNum].style.zIndex="200";
-		if(menuNum == 0)
+		if(menuNum!=-1)
 		{
-			activarmapa();
+			menuElem[menuNum].style.display="none";
+			menuNum = numero;
+			menuElem[menuNum].style.display="grid";
+			document.querySelector("#PantallaSecundaria>.indicador-carpeta>h3").innerHTML=menuElem[menuNum].getAttribute("id");
+			if(menuNum == 0)
+			{
+				activarmapa();
+			}
+		}
+		else
+		{
+			menuNum = numero;
+			pantalla1.style.width="50%";
+			pantalla2.style.width="50%";
+			menuElem[menuNum].style.display="grid";
+			document.querySelector("#PantallaSecundaria>.indicador-carpeta>h3").innerHTML=menuElem[menuNum].getAttribute("id");
+			if(menuNum == 0)
+			{
+				activarmapa();
+			}
+			pantallas.push(numero)
 		}
 	}
 }
+function accionesDeMenu(numero)
+{
+	var indicadorCarpeta = document.querySelector("#PantallaSecundaria>.indicador-carpeta>h3");
+	switch(numero)
+	{
+		case 0:
+			activarmapa();
+			indicadorCarpeta.innerHTML = "Domotica";
+			break;
+		case 1:
+			indicadorCarpeta.innerHTML = "Domotica";
+			break;
+		case 2:
+			indicadorCarpeta.innerHTML = "Domotica";
+			break;
+		case 3:
+			indicadorCarpeta.innerHTML = "Domotica";
+			break;
+	}
+}
 
-/*EVENTOS PARA CONTROLAR EL MAPA*/
-
+//EVENTOS PARA CONTROLAR EL MAPA
 var svg = document.querySelector("#mapa")
 var bordes = document.querySelectorAll("#mapa .borde");
 var parrafos = document.querySelectorAll("#mapa p");
@@ -108,7 +141,7 @@ function desactivarmapa()
 var numeroMarcado = "";
 function aniadirNumeroATelefono(numero)
 {
-	if(document.querySelector('#telefonia .menu-telefono').style.display=="flex" || document.querySelector('#telefonia .menu-telefono').style.display=="")
+	if(document.querySelector('#teléfono .menu-telefono').style.transform=="none" || document.querySelector('#teléfono .menu-telefono').style.transform=="")
 		esconderMenuTelefono();
 	if(numeroMarcado.length%4==0)
 	{
@@ -133,12 +166,12 @@ function quitarNumeroATelefono()
 }
 function esconderMenuTelefono()
 {
-	document.querySelector('#telefonia #btn-borrar').style.display="table";
-	document.querySelector('#telefonia .menu-telefono').style.display="none";
+	document.querySelector('#teléfono #btn-borrar').style.display="table";
+	document.querySelector('#teléfono .menu-telefono').style.transform="translateY(-100%)";
 }
 function mostrarMenuTelefono()
 {
-	document.querySelector('#telefonia #btn-borrar').style.display="none";
-	document.querySelector('#telefonia .menu-telefono').style.display="flex";
+	document.querySelector('#teléfono #btn-borrar').style.display="none";
+	document.querySelector('#teléfono .menu-telefono').style.transform="none";
 }
 mostrarMenuTelefono();
