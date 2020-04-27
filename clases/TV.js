8cb6730fddda19962e3489e687c0e0433b01eed8
Javito774 <javito774@gmail.com>
2020-04-27 19:22:19 +0200

    Funcionalidad TV

--- clases/TV.js
+++ clases/TV.js
@@ -6,24 +6,41 @@ class TV extends Electrodomestico{
 		this.canales = ['La 1','La 2','AtresMedia','Cuatro','Telecinco','La sexta'];
 		this.vol = 0;
 		this.interfaz = document.querySelector(".contenedor-tv");
+		this.intervalo=null;
+		this.numeroMarcado="";
 	}
 	cambiarCanal(canal)
 	{
-		this.canal = canal;
+		this.canal = parseInt(canal);
+		this.interfaz.querySelector(".canal").innerHTML="CH "+ this.canal + " | "+this.canales[this.canal-1];
+	}
+	aniadirNumero(numero)
+	{
+		if(this.numeroMarcado.length<3 && this.estado == "encendida")
+		{
+			clearInterval(this.intervalo);
+			this.numeroMarcado+=numero;
+			this.interfaz.querySelector('.contenedor-numeros p').innerHTML=this.numeroMarcado;
+			this.intervalo = setTimeout(()=>{
+				this.cambiarCanal(this.numeroMarcado);
+				this.numeroMarcado="";
+				this.interfaz.querySelector('.contenedor-numeros p').innerHTML=this.numeroMarcado;
+			},2000);
+		}
 	}
 	subirCanal()
 	{
 		if(this.canal<this.canales.length && this.estado == "encendida"){
 			this.canal++;
-			this.interfaz.querySelector(".canal").innerHTML="CH "+ this.canal + " | "+this.canales[this.canal-1];}
+			this.interfaz.querySelector(".canal").innerHTML="CH "+ this.canal + " | "+this.canales[this.canal-1];
+		}
 	}
 	bajarCanal()
 	{
 		if(this.canal>1 && this.estado == "encendida"){
 			this.canal--;
-
-			this.interfaz.querySelector(".canal").innerHTML="CH "+ this.canal + " | "+this.canales[this.canal-1];}
-
+			this.interfaz.querySelector(".canal").innerHTML="CH "+ this.canal + " | "+this.canales[this.canal-1];
+		}
 	}
 	subirVolumen()
 	{
