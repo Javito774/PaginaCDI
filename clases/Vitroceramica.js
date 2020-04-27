27317f1b6caaf0c9b224035c851bacbc30f1b1e2
Javito774 <javito774@gmail.com>
2020-04-27 10:03:34 +0200

    Añadida funcionalidad visual a la vitro;

--- clases/Vitroceramica.js
+++ clases/Vitroceramica.js
@@ -6,14 +6,6 @@ class Fuego
 		this.estado = 0;
 		this.temporizador=0;
 	}
-	getTemporizador()
-	{
-		return this.temporizador;
-	}
-	setTemporizador(temp)
-	{
-		this.temporizador=temp;
-	}
 	iniciarTemporizador()
 	{
 		this.intervalo = setInterval(()=>{
@@ -37,14 +29,6 @@ class Fuego
 		this.temporizador=0;
 		this.actualizarEstado();
 	}
-	getPotencias()
-	{
-		return this.potencias;
-	}
-	getEstado()
-	{
-		return this.estado;
-	}
 	subirEstado()
 	{
 		if(this.estado<this.potencias)
@@ -65,13 +49,11 @@ class Vitroceramica extends Electrodomestico
 	{
 		super(idAux,"vitroceramica","_ | _ | _","assets/vitrp.png",true);
 		this.interfaz= document.querySelector('.contenedor-vitro');
+		this.interfazfuegos = this.interfaz.querySelectorAll(".cristal .fuego");
+		this.fuegoSeleccionado = -1;
 		this.fuego=new Array(nFuegos);
 		for(var i=0;i<this.fuego.length;i++)
 			this.fuego[i] = new Fuego(potencia);
-	}
-	getFuego(nFuego)
-	{
-		return this.fuego[nFuego];
 	}
 	actualizarEstado()
 	{
@@ -82,6 +64,26 @@ class Vitroceramica extends Electrodomestico
 				this.estado += "_ |";
 			else
 				this.estado +=this.fuego[i].getEstado()+" |";
+		}
+	}
+	seleccionarFuego(numero)
+	{
+		if(this.fuegoSeleccionado==-1)
+		{
+			this.interfaz.querySelector(".controles").style.height = "100%";
+		}
+		else {
+			this.interfazfuegos[this.fuegoSeleccionado].removeAttribute('seleccionado');
+		}
+		if(numero==this.fuegoSeleccionado)
+		{
+			this.interfaz.querySelector(".controles").style.height = "0%";
+			this.fuegoSeleccionado=-1;
+		}
+		else
+		{
+			this.interfazfuegos[numero].setAttribute('seleccionado','');
+			this.fuegoSeleccionado=numero;
 		}
 	}
 }
