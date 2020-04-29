8b4822a171054aec466df5d88ab0d322aa914a5a
Javito774 <javito774@gmail.com>
2020-04-29 12:04:35 +0200

    Funcionalidad de la vitro.

--- clases/Vitroceramica.js
+++ clases/Vitroceramica.js
@@ -7,35 +7,65 @@ class Fuego
 		this.temporizador=0;
 		this.colores = ['#000','#690404','#a40000','#bc0202','#e30404','#ff0000'];
 		this.interfaz = interfaz;
+		this.isPaused = true;
+		this.isSelected=false;
 	}
 	iniciarTemporizador()
 	{
+		document.querySelector(".contenedor-vitro .controles .stop").style.display="block";
+		document.querySelector(".contenedor-vitro .controles .pause").style.display="block";
+		document.querySelector(".contenedor-vitro .controles .play").style.display="none";
+		this.isPaused=false;
 		this.intervalo = setInterval(()=>{
 			if(this.temporizador>0)
 			{
 				this.temporizador--;
-				this.actualizarEstado();
-				console.log(this.estado);
-				this.mostrarTemporizador();
+				if(this.isSelected)
+					this.mostrarTemporizador();
 			}
 			else
+			{
 				clearInterval(this.intervalo);
+				this.estado = 0;
+				this.mostrarInterfaz();
+			}
 		},1000);
 	}
 	pausarTemporizador()
 	{
+		this.isPaused=true;
+		document.querySelector(".contenedor-vitro .controles .stop").style.display="block";
+		document.querySelector(".contenedor-vitro .controles .pause").style.display="block";
+		document.querySelector(".contenedor-vitro .controles .play").style.display="none";
 		clearInterval(this.intervalo);
 	}
 	stopTemporizador()
 	{
+		this.isPaused=true;
 		clearInterval(this.intervalo);
 		this.temporizador=0;
-		this.actualizarEstado();
+		document.querySelector(".contenedor-vitro .minutos").innerHTML="00";
+		document.querySelector(".contenedor-vitro .segundos").innerHTML ="00";
+		document.querySelector(".contenedor-vitro .controles .play").style.display="none";
+		document.querySelector(".contenedor-vitro .controles .pause").style.display="none";
+		document.querySelector(".contenedor-vitro .controles .stop").style.display="none";
 	}
 	mostrarTemporizador()
 	{
 		if(this.temporizador != 0)
 		{
+			if(!this.isPaused)
+			{
+				document.querySelector(".contenedor-vitro .controles .stop").style.display="block";
+				document.querySelector(".contenedor-vitro .controles .pause").style.display="block";
+				document.querySelector(".contenedor-vitro .controles .play").style.display="none";
+			}
+			else
+			{
+				document.querySelector(".contenedor-vitro .controles .stop").style.display="block";
+				document.querySelector(".contenedor-vitro .controles .pause").style.display="none";
+				document.querySelector(".contenedor-vitro .controles .play").style.display="block";
+			}
 			var minutos = parseInt(this.temporizador/60);
 			var segundos = this.temporizador%60;
 			var aux;
@@ -54,8 +84,12 @@ class Fuego
 		}
 		else
 		{
+			document.querySelector(".contenedor-vitro .controles .stop").style.display="none";
+			document.querySelector(".contenedor-vitro .controles .pause").style.display="none";
+			document.querySelector(".contenedor-vitro .controles .play").style.display="none";
 			document.querySelector(".contenedor-vitro .minutos").innerHTML="00";
 			document.querySelector(".contenedor-vitro .segundos").innerHTML ="00";
+			this.isPaused=true;
 		}
 	}
 	subirTiempo()
@@ -85,9 +119,12 @@ class Fuego
 	}
 	mostrarInterfaz()
 	{
-		document.querySelector('.contenedor-vitro .potencia').innerHTML = this.estado;
+		if(this.isSelected)
+		{
+			document.querySelector('.contenedor-vitro .potencia').innerHTML = this.estado;
+			this.mostrarTemporizador();
+		}
 		this.interfaz.style.background="radial-gradient(black, black 28%,white 32%,white 34%, "+this.colores[this.estado]+" 36%, "+this.colores[this.estado]+" 62%, white 67%)";
-		this.mostrarTemporizador();
 	}
 }
 
@@ -116,6 +153,8 @@ class Vitroceramica extends Electrodomestico
 	}
 	mostrarInterfaz()
 	{
+		if(this.fuegoSeleccionado!=-1)
+			electrodomesticoActual=this.fuego[this.fuegoSeleccionado];
 		this.fuego.forEach(fuegu => {
 			fuegu.mostrarInterfaz();
 		});
@@ -128,18 +167,23 @@ class Vitroceramica extends Electrodomestico
 			this.interfaz.querySelector(".controles").style.height = "100%";
 		}
 		else {
+			this.fuego[this.fuegoSeleccionado].isSelected=false;
 			this.interfazfuegos[this.fuegoSeleccionado].removeAttribute('seleccionado');
 		}
 		if(numero==this.fuegoSeleccionado)
 		{
+			electrodomesticoActual=pantallas[pantallas.length-1];
 			this.interfaz.querySelector(".controles").style.height = "0%";
 			this.fuegoSeleccionado=-1;
+			this.fuego[numero].isSelected=false;
 		}
 		else
 		{
 			this.interfazfuegos[numero].setAttribute('seleccionado','');
 			this.fuegoSeleccionado=numero;
+			this.fuego[numero].isSelected=true;
 			this.fuego[numero].mostrarInterfaz();
+			electrodomesticoActual=this.fuego[numero];
 		}
 	}
 }
